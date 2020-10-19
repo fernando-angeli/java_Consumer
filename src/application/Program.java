053ee1e9a646131e2261bcd9e3a8152253d07d5c
Luiz Fernando Angeli <fernando_angeli@hotmail.com>
2020-10-19 00:19:02 -0300

    Expressão Lambda declarada

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,8 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Consumer;
+
 import entities.Product;
 
 public class Program {
@@ -17,6 +19,13 @@ public class Program {
 		list.add(new Product("Mouse",50.00));
 		list.add(new Product("Tablet",350.50));
 		list.add(new Product("HD Case",80.90));
+		
+		//usuário pode definir o fator de ajuste de valor.
+		double factor = 1.1;
+		
+		Consumer<Product> cons = p -> {
+			p.setPrice(p.getPrice() * factor);
+		};
 		
 		list.forEach(Product::nonStaticPriceUpdate);
 
